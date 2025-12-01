945cb7be4c0822ebbde4745dace3d042043d8cc0
Rakibul Islam <rakib.ca@gmail.com>
2025-11-30 20:14:07 -0700

    added sorting component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,15 @@
-import { errorTextEl, errorEl } from '../common.js';
+import {
+    DEFAULT_DISPLAY_TIME,
+    errorTextEl,
+    errorEl
+} from '../common.js';
 
 const renderError = (message = 'Something went wrong') => {
-  errorTextEl.textContent = message;
-  errorEl.classList.add('error--visible');
-  setTimeout(() => {
-    errorEl.classList.remove('error--visible');
-  }, 3500);
+    errorTextEl.textContent = message;
+    errorEl.classList.add('error--visible');
+    setTimeout(() => {
+        errorEl.classList.remove('error--visible');
+    }, DEFAULT_DISPLAY_TIME);
 };
 
 export default renderError;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,11 @@
-import { spinnerSearchEl, spinnerJobDetailsEl } from '../common.js';
+import {
+    spinnerSearchEl,
+    spinnerJobDetailsEl
+} from '../common.js';
 
-const renderSpinner = (whichSpinner) => {
-  const spinnerEl =
-    whichSpinner === 'search' ? spinnerSearchEl : spinnerJobDetailsEl;
-  spinnerEl.classList.toggle('spinner--visible');
+const renderSpinner = whichSpinner => {
+    const spinnerEl = whichSpinner === 'search' ? spinnerSearchEl : spinnerJobDetailsEl;
+    spinnerEl.classList.toggle('spinner--visible');
 };
 
 export default renderSpinner;
