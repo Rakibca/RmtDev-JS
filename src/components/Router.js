c5da7d611b79f5f16196ce4a6a0892495de4901b
Rakibul Islam <rakib.ca@gmail.com>
2026-01-01 22:12:24 -0700

    updated router component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,26 @@
-import { BASE_API_URL, jobDetailsContentEl, getData } from '../common.js';
+import {
+  BASE_API_URL,
+  state,
+  jobDetailsContentEl,
+  getData,
+} from '../common.js';
 import renderSpinner from './Spinner.js';
 import renderJobDetails from './JobDetails.js';
+import renderError from './Error.js';
+import renderJobList from './JobList.js';
 
 const loadHashChangeHandler = async () => {
   // get id from url
   const id = window.location.hash.substring(1);
 
   if (id) {
+    // remove the active class from previously active job items
+    document
+      .querySelectorAll('.job-item--active')
+      .forEach((jobItemWithActiveClass) =>
+        jobItemWithActiveClass.classList.remove('job-item--active')
+      );
+
     // remove previous job details content
     jobDetailsContentEl.innerHTML = '';
 
@@ -19,6 +33,12 @@ const loadHashChangeHandler = async () => {
 
       // extract job item
       const { jobItem } = data;
+
+      // update state
+      state.activeJobItem = jobItem;
+
+      // render search job list
+      renderJobList();
 
       // remove spinner
       renderSpinner('job-details');
