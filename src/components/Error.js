47370cb199e6be27e7cd50a310076ddfeb782e77
Rakibul Islam <rakib.ca@gmail.com>
2026-01-01 22:05:26 -0700

    updated error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,11 @@
-import {
-    DEFAULT_DISPLAY_TIME,
-    errorTextEl,
-    errorEl
-} from '../common.js';
+import { DEFAULT_DISPLAY_TIME, errorTextEl, errorEl } from '../common.js';
 
 const renderError = (message = 'Something went wrong') => {
-    errorTextEl.textContent = message;
-    errorEl.classList.add('error--visible');
-    setTimeout(() => {
-        errorEl.classList.remove('error--visible');
-    }, DEFAULT_DISPLAY_TIME);
+  errorTextEl.textContent = message;
+  errorEl.classList.add('error--visible');
+  setTimeout(() => {
+    errorEl.classList.remove('error--visible');
+  }, DEFAULT_DISPLAY_TIME);
 };
 
 export default renderError;
